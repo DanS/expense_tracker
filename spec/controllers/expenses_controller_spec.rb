3490c588e56445f41b0d585ad3eb778ddadd835d
Dan Steinicke <dansteinicke@gmail.com>
2012-02-18 14:35:03 -0800

    Fix specs broken by requiring User to be associated with Expense

--- spec/controllers/expenses_controller_spec.rb
+++ spec/controllers/expenses_controller_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe ExpensesController do
-  let(:expense) { Factory(:expense) } #make expense memoized method available
+  include Devise::TestHelpers
+  render_views
+
+  before do
+    @user = Factory(:user)
+  end
+  
+  let(:expense) { Factory(:expense, :user_id => @user.id) } #make expense memoized method available
 
   shared_examples_for "finding expense" do
     it "assigns expense" do
@@ -9,135 +16,140 @@ describe ExpensesController do
     end
   end
 
-  describe "GET index" do
-    before do
-      get :index
-    end
+  describe "with signed in user" do
+    before {sign_in @user}
 
-    it "assigns all expenses as @expenses" do
-      assigns(:expenses).should_not be_nil
-    end
-
-    it "renders the index template" do
-      response.should render_template "index"
-    end
-  end
-
-  describe "GET show" do
-    before do
-      get :show, :id => expense.to_param
-    end
-
-    it_should_behave_like "finding expense"
-
-    it "renders the show template" do
-      response.should render_template "show"
-    end
-  end
-
-  describe "GET new" do
-    before { get :new }
-
-    it "assigns a new expense" do
-      assigns(:expense).should be_kind_of Expense
-    end
-
-    it "renders the new template" do
-      response.should render_template "new"
-    end
-  end
-
-  describe "GET edit" do
-    before do
-      get :edit, :id => expense.to_param
-    end
-
-    it_should_behave_like "finding expense"
-
-    it "renders the edit template" do
-      response.should render_template "edit"
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid attributes" do
-      let(:valid_attributes) { Factory.attributes_for(:expense) }
-
-      it "saves a newly created expense" do
-        expect {
-          post :create, :expense => valid_attributes
-        }.to change(Expense, :count).by(1)
+    describe "GET index" do
+      before do
+        get :index
       end
 
-      it "renders the show template" do
-        post :create, :expense => valid_attributes
-        response.should redirect_to expense_path(assigns(:expense).id)
+      it "assigns all expenses as @expenses" do
+        assigns(:expenses).should_not be_nil
       end
 
-      it "sets a flash message" do
-        post :create, :expense => valid_attributes
-        flash[:notice].should == "expense created successfully"
+      it "renders the index template" do
+        response.should render_template "index"
       end
     end
 
-    describe "with invalid attributes" do
-
-      it "saves a newly created expense" do
-        expect {
-          post :create, :expense => {}
-        }.not_to change(Expense, :count)
+    describe "GET show" do
+      before do
+        get :show, :id => expense.to_param
       end
-
-      it "re renders the new template" do
-        post :create, :expense => {}
-        response.should render_template "new"
-      end
-
-      it "sets a flash message" do
-        post :create, :expense => {}
-        flash[:error].should_not be_nil
-      end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      before { put :update, :id => expense.to_param, :expense => {:amount => 20} }
 
       it_should_behave_like "finding expense"
 
-      it "updates the expense" do
-        expense.reload.amount.should == 20
-      end
-
-      it "redirects to the index" do
-        response.should redirect_to expense
+      it "renders the show template" do
+        response.should render_template "show"
       end
     end
 
-    describe "with invalid params" do
-      before { put :update, :id => expense.to_param, :expense => {:amount => nil} }
+    describe "GET new" do
+      before { get :new }
 
-      it "re renders the edit template" do
+      it "assigns a new expense" do
+        assigns(:expense).should be_kind_of Expense
+      end
+
+      it "renders the new template" do
+        response.should render_template "new"
+      end
+    end
+
+    describe "GET edit" do
+      before do
+        get :edit, :id => expense.to_param
+      end
+
+      it_should_behave_like "finding expense"
+
+      it "renders the edit template" do
         response.should render_template "edit"
       end
+    end
 
-      it "sets flash error" do
-        flash[:error].should_not be_nil
+    describe "POST create" do
+      describe "with valid attributes" do
+        let(:valid_attributes) { Factory.attributes_for(:expense, :user_id => @user.id) }
+
+        it "saves a newly created expense" do
+          expect {
+            post :create, :expense => valid_attributes
+          }.to change(Expense, :count).by(1)
+        end
+
+        it "renders the show template" do
+          post :create, :expense => valid_attributes
+          response.should redirect_to expense_path(assigns(:expense).id)
+        end
+
+        it "sets a flash message" do
+          post :create, :expense => valid_attributes
+          flash[:notice].should == "expense created successfully"
+        end
+      end
+
+      describe "with invalid attributes" do
+
+        it "saves a newly created expense" do
+          expect {
+            post :create, :expense => {}
+          }.not_to change(Expense, :count)
+        end
+
+        it "re renders the new template" do
+          post :create, :expense => {}
+          response.should render_template "new"
+        end
+
+        it "sets a flash message" do
+          post :create, :expense => {}
+          flash[:error].should_not be_nil
+        end
+      end
+    end
+
+    describe "PUT update" do
+      describe "with valid params" do
+        before { put :update, :id => expense.to_param, :expense => {:amount => 20} }
+
+        it_should_behave_like "finding expense"
+
+        it "updates the expense" do
+          expense.reload.amount.should == 20
+        end
+
+        it "redirects to the index" do
+          response.should redirect_to expense
+        end
+      end
+
+      describe "with invalid params" do
+        before { put :update, :id => expense.to_param, :expense => {:amount => nil} }
+
+        it "re renders the edit template" do
+          response.should render_template "edit"
+        end
+
+        it "sets flash error" do
+          flash[:error].should_not be_nil
+        end
+      end
+    end
+
+    describe "DELETE destroy" do
+      before { @expense = Factory(:expense) }
+
+      it "deletes the expense" do
+        expect { delete :destroy, :id => @expense.to_param }.to change(Expense, :count).by(-1)
+      end
+
+      it "renders index" do
+        delete :destroy, :id => @expense.to_param
+        response.should redirect_to expenses_path
       end
     end
   end
 
-  describe "DELETE destroy" do
-    before {@expense = Factory(:expense)}
-
-    it "deletes the expense" do
-      expect {delete :destroy, :id => @expense.to_param}.to change(Expense, :count).by(-1)
-    end
-
-    it "renders index" do
-      delete :destroy, :id => @expense.to_param
-      response.should redirect_to expenses_path
-    end
-  end
 end
